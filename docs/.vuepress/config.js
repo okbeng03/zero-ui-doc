9b0d4a5726275e77b6c7c4cd8fc0682eb732c300
chb.wang <chb.wang@hnair.com>
2021-10-27 11:27:06 +0800

    chore: base url

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  title: 'Zero UI',
+  description: '前端中后台配置化UI',
+  base: '/zero-ui-doc/',
+  themeConfig: {
+    nav: [
+      { text: 'Zero UI', link: '/' },
+      { text: 'Form', link: '/form/' },
+      { text: 'Table', link: '/table/' },
+    ],
+    sidebar: {
+      '/form/': [
+        '',
+        'concept',
+        'default',
+        'definition',
+        'component',
+        'validate',
+        'extend',
+        // {
+        //   path: '',
+        //   title: 'Demo 示例',
+        //   children: [
+        //     '/form/example/basic'
+        //   ]
+        // }
+      ],
+      '/table/': [
+        '',
+        'concept',
+        'config',
+        'default',
+        'definition',
+        'component',
+        'search',
+        'extend'
+      ],
+      '/': [
+        '',
+        'start',
+        'component'
+      ]
+    }
+  }
+}
